d840f60c1d388c97cae59b8620e5521c4d53ffc1
Sam Chen <sam8211112222@gmail.com>
2022-08-25 17:48:43 +0800

    新增pairRDD用法

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,55 +13,21 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        List<String> inputData = new ArrayList<>();
+        inputData.add("WARN: Tuesday 4 September 0405");
+        inputData.add("ERROR: Tuesday 4 September 0408");
+        inputData.add("FATAL: Wednesday 5 September 1632");
+        inputData.add("ERROR: Friday 7 September 1854");
+        inputData.add("WARN: Saturday 8 September 1942");
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
-//        Double plusResult = plus(sc);
-//        Double sqrtResult = division(sc);
-//        System.out.println(plusResult);
-        tupleDemp(sc);
-        sc.close();
-    }
 
-    public static Double plus(JavaSparkContext sc){
-        List<Double> inputData = new ArrayList<Double>();
-        inputData.add(34.6);
-        inputData.add(12.4324);
-        inputData.add(3242.32);
-        inputData.add(20.10);
-        JavaRDD<Double> myRdd = sc.parallelize(inputData);
-        Double result = myRdd.reduce((value1,value2) -> value1+value2);
-        return result;
-    }
-
-    public static Double division(JavaSparkContext sc){
-        List<Integer> inputData = new ArrayList<Integer>();
-        inputData.add(34);
-        inputData.add(12);
-        inputData.add(3242);
-        inputData.add(49);
-        JavaRDD<Integer> myRdd = sc.parallelize(inputData);
-        JavaRDD <Double>sqrtRdd = myRdd.map((value) -> Math.sqrt(value));
-        sqrtRdd.foreach(value -> System.out.println(value));
-        //也可以這樣寫
-        sqrtRdd.collect().forEach(System.out::println);
-
-        // 單純用map, reduce可以知道資料筆數
-        JavaRDD<Long> singleIntegerRdd = sqrtRdd.map(value -> 1L);
-        Long count = singleIntegerRdd.reduce((v1,v2) -> v1 + v2);
-        System.out.println(count);
-        return null;
-    }
-
-    public static Double tupleDemp(JavaSparkContext sc){
-        List<Integer> inputData = new ArrayList<Integer>();
-        inputData.add(34);
-        inputData.add(12);
-        inputData.add(3242);
-        inputData.add(49);
-        JavaRDD<Integer> originalIntegers = sc.parallelize(inputData);
-        JavaRDD <Tuple2<Integer,Double>>sqrtRdd = originalIntegers.map(value -> new Tuple2<>(value,Math.sqrt(value)));
-        sqrtRdd.collect().forEach(System.out::println);
-        return null;
+        sc.parallelize(inputData)
+                .mapToPair(rawValue -> new Tuple2<>(rawValue.split(":")[0], 1L))
+                .reduceByKey((value1, value2) -> value1 + value2)
+                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2 + " instances"));
     }
 }
